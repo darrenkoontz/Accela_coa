d941074baebafb493e411e9aee1b54792676df6f
paulhrose <prose@accela.com>
2017-10-26 14:12:52 -0700

    Delete ASA;!~!~!~!~.js
    Uploaded with wrong file name

--- EVENT/ASA;!~!~!~!~.js
+++
@@ -1,9 +0,0 @@
-//Copy Parcel GIS Objects to Record using function copyParcelGisObjects()
-try{
-	if(!publicUser){
-		copyParcelGisObjects();
-	}
-} catch (err) {
-	logDebug("A JavaScript Error occurred: CTRCA:*/*/*/*: copyParcelGisObjects()" + err.message);
-	logDebug(err.stack);
-};
