f904e97ffd99a87b86821279dc9cfa5749b9d18d
Nickie Albert <nalbert@accela.com>
2018-01-31 12:14:47 -0500

    Moved bracket for else condition on finding a ruleset.

--- INCLUDES_CUSTOM/doConfigurableScriptActions.js
+++ INCLUDES_CUSTOM/doConfigurableScriptActions.js
@@ -51,22 +51,23 @@ function doConfigurableScriptActions(){
 		 logDebug("No JSON file exists for this module.");
 	 }else{
 		var configJSON = JSON.parse(configRuleset);
-	 }
+	 
 	
 	// match event, run appropriate configurable scripts
-	settingsArray = [];
-	if(configJSON[controlString]) {
-		var ruleSetArray = configJSON[controlString];
-		var scriptsToRun = ruleSetArray.StandardScripts;
-		
-		for (s in scriptsToRun){
-			logDebug("doConfigurableScriptActions scriptsToRun[s]: " + scriptsToRun[s]);
-			var script = scriptsToRun[s];
-			var validScript = getScriptText(script);
-			if (validScript == ""){
-				logDebug("Configurable script " + script + " does not exist.");
-			}else{
-				eval(getScriptText(scriptsToRun[s]));
+		settingsArray = [];
+		if(configJSON[controlString]) {
+			var ruleSetArray = configJSON[controlString];
+			var scriptsToRun = ruleSetArray.StandardScripts;
+			
+			for (s in scriptsToRun){
+				logDebug("doConfigurableScriptActions scriptsToRun[s]: " + scriptsToRun[s]);
+				var script = scriptsToRun[s];
+				var validScript = getScriptText(script);
+				if (validScript == ""){
+					logDebug("Configurable script " + script + " does not exist.");
+				}else{
+					eval(getScriptText(scriptsToRun[s]));
+				}
 			}
 		}
 	}
