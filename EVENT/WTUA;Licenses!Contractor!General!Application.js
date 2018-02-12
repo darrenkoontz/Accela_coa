b08352e02650f20ff7fd9b68215150ddb16189c3
Nickie Albert <nalbert@accela.com>
2018-02-12 14:34:06 -0500

    Added missing parens

--- EVENT/WTUA;Licenses!Contractor!General!Application.js
+++ EVENT/WTUA;Licenses!Contractor!General!Application.js
@@ -3,7 +3,7 @@
 if(appMatch("Licenses/Contractor/General/Application")){
 	var contType = getAppSpecific("Contractor Type");
 	if(wfTask == "License Issuance" && wfStatus == "Ready to Pay"){
-		if (!matches(contType, "Commercial Building", "Residential Building"){
+		if (!matches(contType, "Commercial Building", "Residential Building")){
 			updateFee("LIC_020", "LIC_CONTRACTOR_GENERAL", "FINAL", 1, "Y");
 		}
 	}
