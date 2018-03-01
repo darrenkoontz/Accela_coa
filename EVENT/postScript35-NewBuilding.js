e61a06087c57a1ab1e34ae8f087cdc6c17dadba8
Nickie Albert <nalbert@accela.com>
2018-03-01 16:18:12 -0500

    Initial

---
+++ EVENT/postScript35-NewBuilding.js
@@ -0,0 +1,5 @@
+activateTask("Water Meter");
+activateTask("Inspection Phase");
+activateTask("Waste Water");
+activateTask("Special Inspection Check");
+activateTask("FEMA Elevation Certification");
