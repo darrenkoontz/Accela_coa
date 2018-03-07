47c718f29ebcadaaaf3d346a5e2ebc75977f3f5f
Main <jmain@auroragov.org>
2018-03-07 13:12:30 -0700

    removed jdtest files

--- INCLUDES_CUSTOM/jdgittestagain.js
+++
@@ -1,4 +0,0 @@
-blah blah blah
-
-here is some new code - this file will be added to the local
-and then the remote repository
--- INCLUDES_CUSTOM/jdtestgit.js
+++
@@ -1,6 +0,0 @@
-//test file - will edit this and test the git push pull mechanism
-
-//here is a new line
-// here is another new line
-
-//here is another line - fun!
