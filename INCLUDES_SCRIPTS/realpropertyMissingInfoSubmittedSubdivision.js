09a19cc29e842df79c8268c3d31fae9267ce2aa9
Main <jmain@auroragov.org>
2018-03-13 08:57:21 -0600

    continued debugging1

--- INCLUDES_SCRIPTS/realpropertyMissingInfoSubmittedSubdivision.js
+++ INCLUDES_SCRIPTS/realpropertyMissingInfoSubmittedSubdivision.js
@@ -12,10 +12,10 @@ logDebug("Start realpropertyMissingInfoSubmittedSubdivision.js")
 
 //get today's date in mm/dd/yyyy format
 var d = new Date;
-month = d.getMonth() + 1;
-day = d.getDate();
-year = d.getFullYear();
-currentdate = month + "/" + day + "/" + year;
+var month = d.getMonth() + 1;
+var day = d.getDate();
+var year = d.getFullYear();
+var currentdate = month + "/" + day + "/" + year;
 logDebug("Today is " + currentdate);
 
 //get five business days from today
@@ -28,21 +28,21 @@ logDebug("Current record Status: " + currentrecordstatus);
 
 //Is this the right record status for this script to do anything?
 var iscorrectrecordstatus = false;
-currentrecordstatus == "Waiting on Documents"; //debug
 if (currentrecordstatus == "Waiting on Documents")
 {
 	iscorrectrecordstatus = true;
 }
-else
-{
-	iscorrectrecordstatus = false;
-}
 logDebug("iscorrectrecordstatus: " + iscorrectrecordstatus);
+
+if (!iscorrectrecordstatus)
+{
+	logDebug("No work to do... Exiting...");
+}
 
 //check if the active task is "Application Acceptance"
 var tasktocheck = "Application Acceptance";
 var iscorrecttask = isTaskActive(tasktocheck);
-logDebug("iscorrecttask " + iscorrecttask);
+logDebug("iscorrecttask: " + iscorrecttask);
 
 //check if the active workflow status is "Missing Information"
 var iscorrecttaskstatus = isTaskStatus(tasktocheck, "Missing Information");
