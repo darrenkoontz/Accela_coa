b0fcc4c11e53fb80f32d1543a54fb360c24ece2a
Warstler <swarstle@auroragov.org>
2018-03-13 17:13:25 -0600

    steves first

---
+++ INCLUDES_SCRIPTS/odaPlanningSupervisorReviewAssigned.js
@@ -0,0 +1,11 @@
+//Script #223
+//Created by SCW 2018-03-13
+//This is SCW's first - does this script even work?
+
+if (wfStatus == "Assigned")
+{
+		
+		editAppSpecific("Number of Failed Inspections", 0);
+		deactivateTask();
+		
+}
