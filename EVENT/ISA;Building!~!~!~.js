d6cefc379598adfc58be315f637823fd2ca86dec
Eric Koontz <EricKoontz@users.noreply.github.com>
2018-03-15 21:14:11 -0600

    Add files via upload

---
+++ EVENT/ISA;Building!~!~!~.js
@@ -0,0 +1,10 @@
+// this scenario could not be added to the configurable script due to the backslash in the result
+
+try {
+		if(inspType == "Sprinkler System" ){
+			updateAppStatus("Awaiting Inspection");
+		}
+	}
+	catch (err) {
+		
+	}
