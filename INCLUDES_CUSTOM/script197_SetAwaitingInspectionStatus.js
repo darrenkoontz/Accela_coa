b95c365a62ca8f2d78ffd7f39c0d072cbd887e49
Eric Koontz <EricKoontz@users.noreply.github.com>
2018-03-15 21:14:57 -0600

    Add files via upload

--- INCLUDES_CUSTOM/script197_SetAwaitingInspectionStatus.js
+++ INCLUDES_CUSTOM/script197_SetAwaitingInspectionStatus.js
@@ -7,3 +7,16 @@
 //              Action update the record status to “Awaiting Inspection”
 //
 //Created By: Silver Lining Solutions
+
+function script197_SetAwaitingInspectionStatus() {
+	logDebug("script197_SetAwaitingInspectionStatus() started.");
+	try {
+		if(inspType == "Sprinkler System" ){
+			updateAppStatus("Awaiting Inspection");
+		}
+	}
+	catch (err) {
+		comment("Error on custom function script197_SetAwaitingInspectionStatus(). Please contact administrator. Err: " + err);
+	}
+
+};//END script197_SetAwaitingInspectionStatus();
