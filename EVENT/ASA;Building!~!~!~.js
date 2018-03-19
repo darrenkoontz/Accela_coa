149ac84dbd9d49a8a3f81f6affeaa1905448fe7c
ChadSLS <24942229+ChadSLS@users.noreply.github.com>
2018-03-19 16:38:42 -0600

    Add files via upload
    script 204

--- EVENT/ASA;Building!~!~!~.js
+++ EVENT/ASA;Building!~!~!~.js
@@ -14,3 +14,4 @@ editAppSpecific("Application Expiration Date",dateAdd(fileDate,180));
 
 script16_FillApplicationNameWhenEmpty();
 script185_UpdateAppExpDate180Days();
+script204_ASAActivateActivateWaterMeterTask();
