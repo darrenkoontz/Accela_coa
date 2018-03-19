931ac8fa31cb119e83cb689c4a710b7dcbb0f08f
ChadSLS <24942229+ChadSLS@users.noreply.github.com>
2018-03-19 17:09:08 -0600

    added code that was in ACP but NOT in gitHUB
    first 5 lines were not in git

--- EVENT/CTRCA;Building!~!~!~.js
+++ EVENT/CTRCA;Building!~!~!~.js
@@ -1,4 +1,8 @@
-
+if (matches(currentUserID,"ADMIN")) {
+showDebug = false;
+showMessage= false;
+}
+include("EMSE:SetContactRelationshipToContactType");
 
 script185_UpdateAppExpDate180Days();
 script204_ASAActivateActivateWaterMeterTask();
