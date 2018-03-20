9ec5f98eb7fe1afccfbbcf971f77116f585cd96f
Eric Koontz <EricKoontz@users.noreply.github.com>
2018-03-20 14:35:58 -0600

    Add files via upload

--- INCLUDES_CUSTOM/script207_SetTotalSqFtOnFireRecord.js
+++ INCLUDES_CUSTOM/script207_SetTotalSqFtOnFireRecord.js
@@ -7,23 +7,26 @@
 //             			and If custom field “Single Family Detached home” = “No” 
 //
 //              Action then auto generate the record “Fire/Primary Inspection/NA/NA” 
-//              		and auto populate the custom field “Building Square Footage” from 
-//						the Building/Permit/New Building/NA record when the workflow status is 
-//						“CO Issued” on the building record. 
+//              		
+//              2nd Criteria workflow status is "Final CO Issued” on the building record
+//              2nd Action populate the custom field “Building Square Footage” from 
+//						the Building/Permit/New Building/NA record  
 //
 //Created By: Silver Lining Solutions
 
 function script207_SetTotalSqFtOnFireRecord() {
 	logDebug("script207_SetTotalSqFtOnFireRecord() started.");
 	try {
-		if(wfTask == "Certificate of Occupancy" && (wfStatus == "Final CO Issued" || wfStatus == "Not Required") )
+		if(wfTask == "Certificate of Occupancy" && (wfStatus == "Final CO Issued" || wfStatus == "Not Required") && )
 			{
 			logDebug("script207_SetTotalSqFtOnFireRecord() passed test.");
 			var cCapId = createChild("Fire", "Primary Inspection", "NA", "NA", ""); 
 			if (cCapId != null) {
-				editAppSpecific("Building Square Footage", {Total Finished Area Sq Ft}, cCapId);
-				logDebug("script207_SetTotalSqFtOnFireRecord() updated ASI.");
-
+				if (wfStatus == "Final CO Issued") 
+					{
+					editAppSpecific("Building Square Footage", {Total Finished Area Sq Ft}, cCapId);
+					logDebug("script207_SetTotalSqFtOnFireRecord() updated ASI.");
+					}
 			}
 		}
 	}
