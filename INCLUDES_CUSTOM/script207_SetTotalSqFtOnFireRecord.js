f726b154407e171cdd69aa4f766f5cb73190bcdf
Eric Koontz <EricKoontz@users.noreply.github.com>
2018-03-20 14:42:24 -0600

    Add files via upload

--- INCLUDES_CUSTOM/script207_SetTotalSqFtOnFireRecord.js
+++ INCLUDES_CUSTOM/script207_SetTotalSqFtOnFireRecord.js
@@ -17,7 +17,7 @@
 function script207_SetTotalSqFtOnFireRecord() {
 	logDebug("script207_SetTotalSqFtOnFireRecord() started.");
 	try {
-		if(wfTask == "Certificate of Occupancy" && (wfStatus == "Final CO Issued" || wfStatus == "Not Required") && )
+		if(wfTask == "Certificate of Occupancy" && (wfStatus == "Final CO Issued" || wfStatus == "Not Required"))
 			{
 			logDebug("script207_SetTotalSqFtOnFireRecord() passed test.");
 			var cCapId = createChild("Fire", "Primary Inspection", "NA", "NA", ""); 
