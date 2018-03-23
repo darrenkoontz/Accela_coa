d29b642a92e13039fbd39bfaa1e70c91bd94a8a6
cweiffenbach <chad@esilverliningsolutions.com>
2018-03-23 09:29:21 -0600

    script 204

---
+++ INCLUDES_CUSTOM/script204_ASAActivateActivateWaterMeterTask.js
@@ -0,0 +1,31 @@
+//Script 204
+//Record Types:	Building/NA/NA/NA
+//Event: 		ApplicationSubmitAfter(Civic Platform) or ConvertToRealCapAfter(ACA)
+//Desc:			If criteria:
+//				If Custom Field Single Family Detached Homes = No
+//				Action: then Activate the Water Meter WF task.
+//Created By: Silver Lining Solutions
+
+function script204_ASAActivateActivateWaterMeterTask() {
+	logDebug("script204_ASAActivateActivateWaterMeterTask started.");
+	try{
+		if(!publicUser){
+			var asiSingleFamilyDetachedHome = AInfo["Single Family Detached Home"];
+		}
+		else {
+			loadAppSpecific4ACA(AInfo);
+			var asiSingleFamilyDetachedHome = AInfo["Single Family Detached Home"];
+		}
+		logDebug("script204: Single Family Detached Home:"+asiSingleFamilyDetachedHome);
+		if (asiSingleFamilyDetachedHome == "No" )
+		{
+				activateTask("Water Meter");
+		}
+	}
+	catch(err){
+		showMessage = true;
+		comment("script204: Error on custom function script204_ASAActivateActivateWaterMeterTask(). Please contact administrator. Err: " + err);
+		logDebug("script204: Error on custom function script204_ASAActivateActivateWaterMeterTask(). Please contact administrator. Err: " + err);
+	}
+	logDebug("script204_ASAActivateActivateWaterMeterTask() ended.");
+};//END script204_ASAActivateActivateWaterMeterTask();
