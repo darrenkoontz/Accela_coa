9ac4b5c9258200746703613991a1a8df95324982
Eric Koontz <EricKoontz@users.noreply.github.com>
2018-03-26 12:04:22 -0600

    Add files via upload

--- INCLUDES_CUSTOM/script175_AssignApplicationSubmittalTask.js
+++ INCLUDES_CUSTOM/script175_AssignApplicationSubmittalTask.js
@@ -3,7 +3,8 @@
 //Event: 		ASA
 //Desc:			Criteria/Action on submittal do:
 // 					1) if submitted via ACA assign WF Task Application Submittal to Anna Bunce
-//                  2) if NOT subed via ACA assign WF Task Application Submittal to the creater of the record
+//                  2) if NOT submitted via ACA assign WF Task Application Submittal to 
+//                     the creator of the record
 //
 //Created By: Silver Lining Solutions
 
@@ -11,8 +12,14 @@ function script175_AssignApplicationSubmittalTask(){
 	logDebug("script175_AssignApplicationSubmittalTask() started.");
 	try
 	{
-		assignTask("Application Submittal","EKOONTZ");
-	}
+		if (publicUser)
+			{
+			assignTask("Application Submittal","ABUNCE");
+			}
+		else 
+			{
+			assignTask("Application Submittal",currentUserID);	
+			}
 	catch(err){
 		logDebug("Error on custom function script175_AssignApplicationSubmittalTask(). Please contact administrator. Err: " + err);
 	}
